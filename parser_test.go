df703a13f7f19bfa334002183b8a9c85fc0bf449
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-03-12 23:46:15 -0300

    Adding K8s file to test

--- parser_test.go
+++ parser_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNECLFileParser(t *testing.T) {
+func TestBasicNECLFileParser(t *testing.T) {
 	file := ParseNECLFile("./test_data/example-1-simple-file.necl")
 
 	// Assert no-block attributes
@@ -24,4 +24,23 @@ func TestNECLFileParser(t *testing.T) {
 	assert.EqualValues(t, []interface{}{"test", "block", "array", 1234, false}, file.Blocks["block"].Attributes["block_array"].Array)
 	assert.EqualValues(t, longArray, file.Attributes["long_array"].Array)
 	assert.EqualValues(t, "this is a blocked multiline string", file.Blocks["block"].Attributes["block_multiline"].Value)
+}
+
+func TestK8sNECLFileParser(t *testing.T) {
+	file := ParseNECLFile("./test_data/example-2-kubernetes-deployment.necl")
+
+	// Global attributes
+	assert.EqualValues(t, "apps/v1", file.Attributes["apiVersion"].Value)
+	assert.EqualValues(t, "Deployment", file.Attributes["kind"].Value)
+
+	// Metadata block
+	assert.EqualValues(t, "nginx-deployment", file.Blocks["metadata"].Attributes["name"].Value)
+	assert.EqualValues(t, "nginx", file.Blocks["metadata"].Blocks["labels"].Attributes["app"].Value)
+
+	// Spec block
+	assert.EqualValues(t, 3, file.Blocks["spec"].Attributes["replicas"].Value)
+	assert.EqualValues(t, "nginx", file.Blocks["spec"].Blocks["selector"].Blocks["matchLabels"].Attributes["app"].Value)
+	assert.EqualValues(t, "nginx", file.Blocks["spec"].Blocks["template"].Blocks["metadata"].Blocks["labels"].Attributes["app"].Value)
+	assert.EqualValues(t, "nginx:1.14.2", file.Blocks["spec"].Blocks["template"].Blocks["spec"].Blocks["containers"].Blocks["nginx"].Attributes["image"].Value)
+	assert.EqualValues(t, 80, file.Blocks["spec"].Blocks["template"].Blocks["spec"].Blocks["containers"].Blocks["nginx"].Blocks["ports"].Attributes["containerPort"].Value)
 }
