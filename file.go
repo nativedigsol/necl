a802d2aaa1ce5f1c31d2f0880891ddd125c99eb4
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-03-12 23:46:06 -0300

    Removing RawText field + Blocks should be a map

--- file.go
+++ file.go
@@ -3,14 +3,12 @@ package necl
 type File struct {
 	Attributes map[string]Attribute
 	Blocks     map[string]Block
-	RawText    []string
 }
 
 type Block struct {
 	Name       string
 	Attributes map[string]Attribute
-	Blocks     []Block
-	RawText    []string
+	Blocks     map[string]Block
 }
 
 type Attribute struct {
