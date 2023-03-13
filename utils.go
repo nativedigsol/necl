266d1d7d4b46e3f1cb0be8adef9022015e74b5dc
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-03-13 12:46:32 -0300

    ContainsMany

--- utils.go
+++ utils.go
@@ -1,7 +1,14 @@
 package necl
 
-func CheckErr(e error) {
-	if e != nil {
-		panic(e)
+import "strings"
+
+// ContainsMany will compare a target string with multiple strings
+func ContainsMany(target string, compareTo []string) bool {
+	for _, compare := range compareTo {
+		if strings.Contains(target, compare) {
+			return true
+		}
 	}
+
+	return false
 }
